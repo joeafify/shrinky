3496bb9c2dce1cd45fb4550e96d8285a0cf20899
Dev Joe <joe.afify95@gmail.com>
2021-02-04 03:13:00 +0200

    Adding optimizeImg function

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const { app, BrowserWindow, Menu, globalShortcut, ipcMain } = require("electron");
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  globalShortcut,
+  ipcMain,
+  shell,
+} = require("electron");
+const path = require("path");
+const os = require("os");
+const imagemin = require("imagemin");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminPngquant = require("imagemin-pngquant");
+const slash = require("slash");
 
 const isDev = (process.env.NODE_ENV = "development" ? true : false);
 const isMac = process.platform === "darwin" ? true : false;
@@ -9,7 +22,7 @@ function createMainWindow() {
   mainWindow = new BrowserWindow({
     width: 500,
     height: 600,
-    resizable:false,
+    resizable: false,
     icon: "./assets/images/shrink.png",
     webPreferences: {
       nodeIntegration: true,
@@ -77,9 +90,24 @@ if (isMac) {
   menuTemplate.unshift({ role: app.name });
 }
 
-ipcMain.on("image:minimizer", (e, options) => {
-  
-})
+ipcMain.on("image:minimize", (e, { imgPath, quality }) => {
+  const dest = path.join(os.homedir(), "shrinky", "output");
+  optimizeImg(imgPath, quality, dest);
+});
+async function optimizeImg(imgPath, quality, dest) {
+  try {
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({ quality: [quality / 100, quality / 100] }),
+      ],
+    });
+    shell.openItem(dest);
+  } catch (error) {
+    console.log(error);
+  }
+}
 app.on("window-all-closed", () => {
   if (!isMac) {
     app.quit();
